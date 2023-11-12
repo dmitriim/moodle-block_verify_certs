dbd70d56aa1d660ff2026399bd88c092f3a33036
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2023-11-13 00:05:31 +1100

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_verify_certs';
-$plugin->release = '0.1.0';
-$plugin->version = 2023110800;
+$plugin->release = 2023111300;
+$plugin->version = 2023111300;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 403];
